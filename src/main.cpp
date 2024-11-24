a627c12f5b0ddb09c2f5e3f68750a298cdfc1d5f
Sipos Attila <sattipolo@gmail.com>
2024-11-24 11:09:42 +0100

    TreeString to cout.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
 #include <testme_input/code_input.hpp>
 
 int main() {
   CodeInput input("data/input/example.cs");
   input.processInput();
+  std::cout << input.getTreeString() << std::endl;
 
   return 0;
 }
